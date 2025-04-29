3b1418c764d6128f41dd5c215d730a940cac14dc
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2025-04-29 19:16:45 +0300

    if (!__instance.IsPlayer) return;

--- CSharp/Shared/Patches/CharacterCanInteractWith.cs
+++ CSharp/Shared/Patches/CharacterCanInteractWith.cs
@@ -9,6 +9,19 @@ using HarmonyLib;
 using Microsoft.Xna.Framework;
 using QIDependencyInjection;
 
+using Barotrauma.Abilities;
+using Barotrauma.Extensions;
+using Barotrauma.IO;
+using Barotrauma.Items.Components;
+using Barotrauma.Networking;
+using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+using System.Diagnostics;
+using System.Xml.Linq;
+#if SERVER
+using System.Text;
+#endif
+
 namespace QuickInteractions
 {
   [PatchClass]
@@ -38,7 +51,7 @@ namespace QuickInteractions
           typeof(bool),
           typeof(bool),
           }),
-          prefix: new HarmonyMethod(typeof(CanInteractWith).GetMethod("Character_CanInteractWith_Prefix"))
+          prefix: new HarmonyMethod(typeof(CanInteractWith).GetMethod("Character_CanInteractWith_Replace"))
         );
       }
       catch (Exception e)
@@ -47,19 +60,42 @@ namespace QuickInteractions
       }
     }
 
-    public static bool Character_CanInteractWith_Prefix(Character __instance, ref bool __result, Character c, float maxDist = 200.0f, bool checkVisibility = true, bool skipDistanceCheck = false)
+    public static bool Character_CanInteractWith_Replace(Character __instance, ref bool __result, Character c, float maxDist = 200.0f, bool checkVisibility = true, bool skipDistanceCheck = false)
     {
       if (GhostDetector.Check()) return true;
+
       if (c == __instance || __instance.Removed || !c.Enabled || !c.CanBeSelected || c.InvisibleTimer > 0.0f)
       {
         __result = false; return false;
       }
-      __result = true; return false;
+
+      if (!c.CharacterHealth.UseHealthWindow && !c.IsDraggable && (c.onCustomInteract == null || !c.AllowCustomInteract))
+      {
+        __result = false; return false;
+      }
+
+      if (__instance.IsPlayer) { __result = true; return false; }
+
+      if (!skipDistanceCheck)
+      {
+        maxDist = Math.Max(ConvertUnits.ToSimUnits(maxDist), c.AnimController.Collider.GetMaxExtent());
+        if (Vector2.DistanceSquared(__instance.SimPosition, c.SimPosition) > maxDist * maxDist &&
+            Vector2.DistanceSquared(__instance.SimPosition, c.AnimController.MainLimb.SimPosition) > maxDist * maxDist)
+        {
+          __result = false; return false;
+        }
+      }
+
+      __result = !checkVisibility || __instance.CanSeeTarget(c);
+      return false;
     }
 
     public static void Character_CanInteractWith_Postfix(Character __instance, ref bool __result, Item item)
     {
       if (GhostDetector.Check()) return;
+
+      if (!__instance.IsPlayer) return;
+
       if (item == Fabricators?.OutpostFabricator) __result = true;
       if (item == Fabricators?.OutpostDeconstructor) __result = true;
       if (item == Fabricators?.OutpostMedFabricator) __result = true;
